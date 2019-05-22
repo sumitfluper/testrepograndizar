3520b498957f98d73c93f98b874e4c8a50488998
kamal sharma <sharmakamal487@gmail.com>
2019-05-22 18:35:43 +0530

    firbase code update

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -96,6 +96,7 @@ exports.userSignup = (req, res) => {
                         //var country_code = userData.country_code;
                         var to = userData.country_code + userData.mobile_number;
                         commFunc.sendotp(verification_code, to);
+                        
                         commFunc.createFirebaseNode(id,userName,profileImage);
                         res.status(200).json({
                             message: "SignUp successfull and OTP successfully sent",
